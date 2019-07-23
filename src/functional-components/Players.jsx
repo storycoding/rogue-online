6f0fb9cb1106430fb9cc9a31fa92bad2123bafb7
Nuno Neves <youknownuno@gmail.com>
2019-07-23 22:03:55 +0200

    updated sprites, created dynamic imports in react component

--- src/functional-components/Players.jsx
+++ src/functional-components/Players.jsx
@@ -3,11 +3,16 @@ import React from "react";
 const Players = (props) => {
   return Object.keys(props.players).map( id => {
     const player = props.players[id];
+
+    player.sprite = "frog"; // will be sourced from player data
+
     const playerStyle = {
       gridColumn: `${player.location.c+1}`,
       gridRow: `${player.location.r+1}`,
     };
-    return <div className="sprite" key={id.slice(0,7)} style={playerStyle}></div>
+  
+
+    return <div className={`sprite ${player.sprite}`} key={id.slice(0,7)} style={playerStyle}></div>
   });
 }
 
