aa6ac5a3d85a7f75e2416d6f40e7212bd8ec6e06
Nuno Neves <youknownuno@gmail.com>
2019-07-30 16:24:35 +0200

    added touch to keypress conversion algorithm

--- src/stateful-components/app.jsx
+++ src/stateful-components/app.jsx
@@ -5,6 +5,8 @@ import Blocks from "../functional-components/Blocks.jsx";
 import Players from "../functional-components/Players.jsx";
 
 import inputKeys from "../static/input-keys";
+import mapTouchToKey from "../events/mapTouchToKey";
+
 import subscribeToSocketIo from "../api/subscribe-to-socket-io";
 
 const port = process.env.PORT || 3000;
@@ -24,6 +26,7 @@ class App extends Component {
     subscribeToSocketIo(client, this);
 
     this.keyPress = this.keyPress.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   keyPress({key}) {
@@ -32,8 +35,34 @@ class App extends Component {
     }
   }
 
+  handleClick(clickEvent) {
+    const key = mapTouchToKey(clickEvent, this);
+    return key ? this.keyPress({key}) : null;
+  }
+
   componentDidMount() {
     document.addEventListener("keydown", this.keyPress);
+    document.addEventListener("click", this.handleClick);
+  }
+
+  componentDidUpdate() {
+    if ( 
+      this.state.player.id &&
+      this.state.players[this.state.player.id] &&
+      !this.state.player.div 
+      ) {
+
+      const playerDiv = document.getElementById(this.state.player.id);
+      
+      const newState = {
+        player: { 
+          ...this.state.player,
+          div: playerDiv,
+        }
+      };
+
+      this.setState(newState);
+    }
   }
 
   render() {
