4df6ae173beb3d2d920a949cb003c9d9554a1e14
storycoding <youknownuno@gmail.com>
2019-08-21 12:48:19 +0200

    added new socket events to the reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -4,6 +4,18 @@ const reducer = (state, action) => {
       state.client.emit("request-game-state");
       return state;
 
+    case 'REQUEST_GAME_GRID' :
+      state.client.emit("request-game-grid");
+      return state;
+    
+    case 'REQUEST_GAME_PLAYERS' :
+      state.client.emit("request-game-players");
+      return state;
+    
+    case 'REQUEST_GAME_CURRENT_PLAYER' :
+      state.client.emit("request-game-current-player");
+      return state;
+
     case 'REQUEST_MOVEMENT_TO_DIRECTION' :
       state.client.emit("request-movement-to-direction", action.payload);
       return state;
